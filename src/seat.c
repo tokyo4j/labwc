bf6b3c018d11d56295bc1e4ba2d07005716b26ba
tokyo4j <hrak1529@gmail.com>
2025-01-24 16:44:51 +0900

    Revert "seat: prevent xwayland from using incorrect keymap (#1816)"
    This reverts commit ad15c0474db518ece55846ef624fa2d85358d89e.
    
    This is required for Fcitx5 to see the correct modifier state when it
    grabs the keyboard. Previously, the active keyboard on the seat
    (wlr_seat.keyboard_state.keyboard) can be NULL when a virtual keyboard
    from Fcitx5 is destroyed, which makes it impossible for Fcitx5 to see the
    modifier state of the physical keyboard when it grabs the keyboard again.
    
    This revert reintroduces the issue of xwayland clients using incorrect
    keymap when keycodes are injected like "wtype a". However, this seems to
    be a fundamental issue with X11 that clients cannot get the keymap exactly
    at the time when keycodes are received, and is a common problem among
    other compositors including sway, wayfire and Hyprland. Specifically for
    wtype, the issue can be mitigated by delaying destroying the virtual
    keyboard like "wtype -k a -s 200".

--- src/seat.c
+++ src/seat.c
@@ -31,6 +31,19 @@ input_device_destroy(struct wl_listener *listener, void *data)
 		wl_list_remove(&keyboard->key.link);
 		wl_list_remove(&keyboard->modifier.link);
 		keyboard_cancel_keybind_repeat(keyboard);
+		/*
+		 * If the active keyboard on the seat is destroyed, fall back
+		 * to the keyboard from keyboard group so we can respond to
+		 * wl_seat.get_keyboard requests with wl_keyboard.keymap event.
+		 * This prevents Chromium from crashing when started just after
+		 * the active keyboard is destroyed.
+		 */
+		struct wlr_keyboard *active_keyboard =
+			wlr_seat_get_keyboard(input->seat->seat);
+		if (!active_keyboard || active_keyboard == keyboard->wlr_keyboard) {
+			wlr_seat_set_keyboard(input->seat->seat,
+				&input->seat->keyboard_group->keyboard);
+		}
 	}
 	free(input);
 }
@@ -694,15 +707,6 @@ seat_focus(struct seat *seat, struct wlr_surface *surface,
 		wlr_seat_keyboard_notify_clear_focus(seat->seat);
 		input_method_relay_set_focus(seat->input_method_relay, NULL);
 		return;
-	}
-
-	if (!wlr_seat_get_keyboard(seat->seat)) {
-		/*
-		 * wlr_seat_keyboard_notify_enter() sends wl_keyboard.modifiers,
-		 * but it may crash some apps (e.g. Chromium) if
-		 * wl_keyboard.keymap is not sent beforehand.
-		 */
-		wlr_seat_set_keyboard(seat->seat, &seat->keyboard_group->keyboard);
 	}
 
 	/*
