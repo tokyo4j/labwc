tokyo4j/labwc

commit 885919fc5fce2df9bf178204b1c72d4c2d6f8b45

cursor: let followMouse update focus only when cursor enters a surface
Before this patch, when followMouse and followMouseRequiresMovement are
both yes, we set the keyboard focus when the cursor moves within an
unfocused surface. However, kwin, xfwm4 and openbox all set keyboard focus
only when the cursor enters a surface.

src/input/cursor.c
@@ -599,11 +599,6 @@ cursor_process_motion(struct server *server, uint32_t time, double *sx, double *
 		dnd_icons_move(seat, seat->cursor->x, seat->cursor->y);
 	}
 
-	if ((ctx.view || ctx.surface) && rc.focus_follow_mouse) {
-		desktop_focus_view_or_surface(seat, ctx.view, ctx.surface,
-			rc.raise_on_focus);
-	}
-
 	struct mousebind *mousebind;
 	wl_list_for_each(mousebind, &rc.mousebinds, link) {
 		if (mousebind->mouse_event == MOUSE_ACTION_DRAG
@@ -619,8 +614,27 @@ cursor_process_motion(struct server *server, uint32_t time, double *sx, double *
 		}
 	}
 
-	return cursor_update_common(server, &ctx, time,
+	struct wlr_surface *old_focused_surface =
+		seat->seat->pointer_state.focused_surface;
+
+	bool notify = cursor_update_common(server, &ctx, time,
 		/* cursor_has_moved */ true, sx, sy);
+
+	struct wlr_surface *new_focused_surface =
+		seat->seat->pointer_state.focused_surface;
+
+	if (rc.focus_follow_mouse && new_focused_surface
+			&& old_focused_surface != new_focused_surface) {
+		/*
+		 * If followMouse=yes, update the keyboard focus when the
+		 * cursor enters a surface
+		 */
+		desktop_focus_view_or_surface(seat,
+			view_from_wlr_surface(new_focused_surface),
+			new_focused_surface, rc.raise_on_focus);
+	}
+
+	return notify;
 }
 
 static uint32_t

commit ddcbac21e689463bb8f293254f5d190cc1beeea1

cursor: update focus on entering SSD if followMouse=yes
This fixes a known regression in 885919fc that cursor entering the
titlebar (and other SSD parts) doesn't update the keyboard focus even when
followMouse=yes.

include/labwc.h
@@ -82,6 +82,9 @@ struct seat {
 	 */
 	struct cursor_context_saved pressed;
 
+	/* Cursor context of the last cursor motion */
+	struct cursor_context_saved hovered;
+
 	struct lab_set bound_buttons;
 
 	struct {

src/desktop.c
@@ -118,11 +118,10 @@ void
 desktop_focus_view_or_surface(struct seat *seat, struct view *view,
 		struct wlr_surface *surface, bool raise)
 {
-	assert(view || surface);
 	if (view) {
 		desktop_focus_view(view, raise);
 #if HAVE_XWAYLAND
-	} else {
+	} else if (surface) {
 		struct wlr_xwayland_surface *xsurface =
 			wlr_xwayland_surface_try_from_wlr_surface(surface);
 		if (xsurface && wlr_xwayland_surface_override_redirect_wants_focus(xsurface)) {

src/input/cursor.c
@@ -604,25 +604,29 @@ cursor_process_motion(struct server *server, uint32_t time, double *sx, double *
 		}
 	}
 
-	struct wlr_surface *old_focused_surface =
-		seat->seat->pointer_state.focused_surface;
-
 	struct cursor_context new_ctx = {0};
 	cursor_update_common(server, &ctx, &new_ctx);
 
-	struct wlr_surface *new_focused_surface =
-		seat->seat->pointer_state.focused_surface;
-
-	if (rc.focus_follow_mouse && new_focused_surface
-			&& old_focused_surface != new_focused_surface) {
+	if (rc.focus_follow_mouse) {
 		/*
-		 * If followMouse=yes, update the keyboard focus when the
-		 * cursor enters a surface
+		 * If followMouse=yes, entering a surface or view updates
+		 * keyboard focus. Note that moving the cursor between a
+		 * surface and a SSD within the same view doesn't update
+		 * keyboard focus, and that entering a surface/view doesn't
+		 * update keyboard focus if implicit grab is active.
 		 */
-		desktop_focus_view_or_surface(seat,
-			view_from_wlr_surface(new_focused_surface),
-			new_focused_surface, rc.raise_on_focus);
+		bool entering = false;
+		if (new_ctx.view) {
+			entering = new_ctx.view != seat->hovered.ctx.view;
+		} else if (new_ctx.surface) {
+			entering = new_ctx.surface != seat->hovered.ctx.surface;
+		}
+		if (entering) {
+			desktop_focus_view_or_surface(seat, new_ctx.view,
+				new_ctx.surface, rc.raise_on_focus);
+		}
 	}
+	cursor_context_save(&seat->hovered, &new_ctx);
 
 	*sx = new_ctx.sx;
 	*sy = new_ctx.sy;