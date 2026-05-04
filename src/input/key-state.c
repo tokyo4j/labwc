98b3d911eb56f54794889afca9ec7aeb7d5862cf
tokyo4j <hrak1529@gmail.com>
2026-05-04 14:27:06 +0900

    Remove key state debugging by LABWC_DEBUG_KEY_STATE env var
    I think this is now less demanded due to the introduction of
    DebugToggleKeyStateIndicator action.

--- src/input/key-state.c
+++ src/input/key-state.c
@@ -169,39 +169,14 @@ key_state_indicator_toggle(void)
 	show_debug_indicator = !show_debug_indicator;
 }
 
-static void
-report(struct lab_set *key_set, const char *msg)
-{
-	static char *should_print;
-	static bool has_run;
-
-	if (!has_run) {
-		should_print = getenv("LABWC_DEBUG_KEY_STATE");
-		has_run = true;
-	}
-	if (!should_print) {
-		return;
-	}
-	printf("%s", msg);
-	for (int i = 0; i < key_set->size; ++i) {
-		printf("%d,", key_set->values[i]);
-	}
-	printf("\n");
-}
-
 uint32_t *
 key_state_pressed_sent_keycodes(void)
 {
-	report(&pressed, "before - pressed:");
-	report(&bound, "before - bound:");
-
 	/* pressed_sent = pressed - bound */
 	pressed_sent = pressed;
 	for (int i = 0; i < bound.size; ++i) {
 		lab_set_remove(&pressed_sent, bound.values[i]);
 	}
-
-	report(&pressed_sent, "after - pressed_sent:");
 
 	return pressed_sent.values;
 }
